ea5d56e459c02f7237a877eac7ef309b2c710378
Marlon O. Rodriguez <mrod976@gmail.com>
2015-03-05 15:44:27 -0500

    more coding added
    added some conditions for the data

--- Rodriguez_Marlon_Output_Assignment/Creating Variables and Output.js
+++ Rodriguez_Marlon_Output_Assignment/Creating Variables and Output.js
@@ -11,7 +11,7 @@ var visitorsName = window.prompt("Please enter you name:");   //visitor will be 
 
 //Getting host and visitor gender
 var hostGender = "Male";
-var getVisitorsGender = window.alert("are you Male ?");
+var getVisitorsGender = alert("are you Male ?");
 if(getVisitorsGender == true){
 	var visitorsGender = "Male"
 }else{
@@ -19,11 +19,11 @@ if(getVisitorsGender == true){
 	}
 
 //Getting host and visitor state of residence
-var hostStateOfResidence = " New York ";
-var getVisitorsStateOfResidence = window.prompt("What state do you live in ?");
+var hostStateOfResidence = "New York";
+var visitorsStateOfResidence = window.prompt("What state do you live in ?");
 	
 //Getting host and visitor profession
-var hostProfession = "IT specialist/Jr Web Developer";
+var hostProfession = "IT specialist";
 var visitorsProfession = window.prompt("Please enter your profession:"); //visitor will be asked to type his/her profession
 
 //Getting host and visitor age
@@ -48,13 +48,55 @@ if ((getVisitorsChildren == 'yes')||(getVisitorsChildren == 'YES')){
 }
 
 
+	
+
+//DOCUMENT OUTPUT PARAGRAPH
 document.writeln("Hello there, I am " + hostName + " Welcome to my page " + visitorsName);
 document.write("<br>");
 document.writeln("I am an " + hostProfession + " who loves Technology and Web ");
 document.write("<br>")
-if(visitorsProfession === hostProfession){
-	document.writeln("I see that you are working in the same field as me Colleague, nice to meet you");
+
+//comparing Professions
+if(hostProfession == visitorsProfession){
+	document.writeln("I am glad that you are also a " + hostProfession + " together we can build some projects");
+}
+else{
+	document.writeln("It is very nice to meet a " + visitorsProfession + ", you never know when you might need an expert");
+}
+document.write("<br>")
+
+//comparing Place of residence
+if(visitorsStateOfResidence == hostStateOfResidence ){
+	document.writeln("So you also live in " + hostStateOfResidence + ", tell me how do you deal with this snow");
 }else{
-	document.writeln("Very interesting to meet people with your career, you never know when you would need an expert");
+	document.writeln("I hope that in " + visitorsStateOfResidence + ", you can enjoy a better weather");
+}
+document.write("<br>")
+
+//comparing Ages
+if(hostAge < visitorsAge){
+	document.writeln("I see that you are " + visitorsAge + " years old, so you have to have tons of great stories");
+}else if(hostAge > visitorsAge){
+	document.writeln("I am older than you and I can tell that when I was " + visitorsAge + " I wanted to reinvent the wheel");
+}else{
+	document.writeln("Oh, so you are the same age as I am. well we are probably twins then, lol ");
+}
+document.write("<br>")
+
+//comparing number of children
+if(visitorsNumberOfKids > hostNumberOfKids){
+		document.writeln("I have only " + hostNumberOfKids + " kid, but Wow you have " + visitorsNumberOfKids + ", I bet your days need around 40 hours");
+}else if(visitorsNumberOfKids == hostNumberOfKids){
+	    document.writeln("I see you have " + hostNumberOfKids + " kid, don't you think is better for the pocket");
+}else{
+	   document.writeln("I guess you don't know what it is to meet an angel");
+}
+document.write("<br>")
+//Ending Paragraph
+if(visitorsGender == hostGender){
+	document.writeln("Well It was a pleasure to see you around my site, one of this days we could have a beer!");
+}else{
+	document.writeln("Well It was a pleasure to see you around my site, one of this days we could have dinner!");
 }
 
+	    
