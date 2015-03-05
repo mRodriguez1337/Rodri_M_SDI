84c8f394e475b71508d86b1cdb64a088100c1179
Marlon O. Rodriguez <mrod976@gmail.com>
2015-03-05 14:02:05 -0500

    Some Changes to Comments
    Added more variables and data

--- Rodriguez_Marlon_Output_Assignment/Creating Variables and Output.js
+++ Rodriguez_Marlon_Output_Assignment/Creating Variables and Output.js
@@ -3,23 +3,43 @@ Scalable Data Infrastructures - Sec 02
 Professor Lee Lewis
 created by Marlon Rodriguez */
 
-//Variables declaration,here you can find all global variables declared 
-
+//Variables declaration,here you can find all global variables declared
+ 
+//Getting host and visitor names
 var hostName = "Marlon Rodriguez";
 var visitorsName = window.prompt("Please enter you name:");   //visitor will be asked to type his/her name
+
+//Getting host and visitor gender
+var hostGender = "Male";
+var getVisitorsGender = window.alert("are you Male ?");
+if(getVisitorsGender == true){
+	var visitorsGender = "Male"
+}else{
+	visitorsGender = "Female"
+	}
+
+//Getting host and visitor state of residence
+var hostStateOfResidence = " New York ";
+var getVisitorsStateOfResidence = window.prompt("What state do you live in ?");
+	
+//Getting host and visitor profession
 var hostProfession = "IT specialist/Jr Web Developer";
 var visitorsProfession = window.prompt("Please enter your profession:"); //visitor will be asked to type his/her profession
+
+//Getting host and visitor age
 var hostAge = 38;
 var getVisitorsAge = window.prompt("Please enter your age:"); //visitor will be asked to type his/her age
 var visitorsAge = Number(getVisitorsAge);                     //visitor age will be convert to number type
+
+//Getting host and visitor info about children
 var hostChildren = true ;
 var hostNumberOfKids = 1 ;
-var getVisitorsChildren = window.prompt("Do you have kids ? ");
+var getVisitorsChildren = window.prompt("Do you have kids ? ");  //visitor will be asked if they have children
 
 if ((getVisitorsChildren == 'yes')||(getVisitorsChildren == 'YES')){
     
     var visitorsChildren = true;
-    var getNumberOfKids = window.prompt("How many children do you have?");
+    var getNumberOfKids = window.prompt("How many children do you have?");  //number of visitor's children
     var visitorsNumberOfKids = Number(getNumberOfKids);
         
 } else if((getVisitorsChildren == 'no')||(getVisitorsChildren == 'NO')){
@@ -29,4 +49,5 @@ if ((getVisitorsChildren == 'yes')||(getVisitorsChildren == 'YES')){
 
 
 document.writeln("Hello there, I am " + hostName + " Welcome to my page " + visitorsName);
+document.write("<br>");
 document.writeln("I am an " + hostProfession + " who loves Technology and Web ");
