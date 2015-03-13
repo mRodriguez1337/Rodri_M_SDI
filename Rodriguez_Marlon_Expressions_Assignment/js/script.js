206c24646acee6afdfc7a84b053d8c8e82320e57
Marlon O. Rodriguez <mrod976@gmail.com>
2015-03-12 22:03:39 -0400

    adding some style
    adding some style codes to page

--- Rodriguez_Marlon_Expressions_Assignment/js/script.js
+++ Rodriguez_Marlon_Expressions_Assignment/js/script.js
@@ -1,1 +1,15 @@
-// JavaScript Document
+/*  JavaScript Document
+Scalable Data Infrastructures Sec 02
+Expressions Assignment
+Professor Lee Lewis
+created by Marlon Rodriguez
+*/
+
+//Creating Variables
+var total = 0 ;
+var apples = 0, mangos = 0, oranges = 0, papayas = 0, pears = 0, straw = 0;
+var carrots = 0, cucum = 0, peppers = 0, plantains = 0, potatoes = 0, tomatoes = 0;
+var costOfFruits = [];
+var costOfVegetables = [];
+ 
+ 
