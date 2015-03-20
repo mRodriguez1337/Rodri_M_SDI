4f3d5f1cb665c5b1671bd2de5980cf6a4c157d23
Marlon O. Rodriguez <mrod976@gmail.com>
2015-03-19 21:04:09 -0400

    added some HTML and CSS
    changed size of products pics and changed some styles

---
+++ Rodriguez_Marlon_Conditionals_Assignment/js/script.js
@@ -0,0 +1,10 @@
+/*  JavaScript Document
+Scalable Data Infrastructures Sec 02
+Conditionals Assignment
+Professor Lee Lewis
+created by Marlon Rodriguez
+*/
+
+//Variables Declaration
+
+var customerName 
