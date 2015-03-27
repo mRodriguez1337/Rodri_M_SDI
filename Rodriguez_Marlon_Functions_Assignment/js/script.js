6d008effcedcab4ba9f2cc425e4501282925b4b8
Marlon O. Rodriguez <mrod976@gmail.com>
2015-03-26 21:22:52 -0400

    Creating Functions
    creating functions to generate random numbers

--- Rodriguez_Marlon_Functions_Assignment/js/script.js
+++ Rodriguez_Marlon_Functions_Assignment/js/script.js
@@ -6,28 +6,68 @@ created by Marlon Rodriguez
 */
 
 //Variables Declaration
-var myName = prompt("Enter your name please: ");
+var myName; 
+var wantToPlay;
+var playingNums;
 
-
-//Functions
-function nameValidation(firstName) {
+//Functions Declarations
+// Validating Name of the User
+function nameValidation(firstName, flag) {
 	
 	           var timesClicked = 1;
-
                while (firstName === ""){
                                      firstName = prompt("Do not leave this blank. \n\n Please enter your first name");
 									 timesClicked++;
-									 if(timesClicked === 10) {
-										 console.log("No time for Playing Buddy !!");
-										 break;
-									 
-									 
-									 }
-}
+									 if(timesClicked === 5) {
+										 console.log("Thank you for visiting our lottery website,\ncomeback when you decide to play !");
+										 flag = 1;
+										 break;	
+										   }
+                                       }
                   return firstName;
+				  
 }
+
+//Validating If User Wants to Play the Lottery
+
+function playValidation(confirmPlay){
+					
+						var timesClicked = 1						
+						           while (confirmPlay === ""){
+                                     confirmPlay = prompt("Do not leave this blank. \nPlease confirm if you want to play: ");
+									 timesClicked++;
+									 if(timesClicked === 5) {
+										 console.log("Thank you for visiting our lottery website,\ncomeback when you decide to play !");
+										 break;
+										             }
+                                                   }
+									return confirmPlay;			   
+}
+
+//Generating Random Numbers
+function randomNumGen(max, min, num) {
+					
+					var randomArray = [];
+					for(var i = 0; i < num; i++) {
+	                  
+					     var myRandomNumGen = Math.round(Math.random() * (max - min) + min);
+						 randomArray[i] = myRandomNumGen;
+					                              }
+												  
+						return randomArray;	
+}
+
+
 
 
 //Main code
+/*myName = prompt("Enter your name please: ");
 myName = nameValidation(myName);
-console.log("Hello, " + myName);
+console.log(myName);
+
+wantToPlay = prompt("Do you want to play the lottery ? ");
+wantToPlay = playValidation(wantToPlay);
+console.log(wantToPlay); */
+
+playingNums = randomNumGen(53, 1, 5);
+console.log(playingNums);
